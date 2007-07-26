891519c2f6deef59cf4d2e9e90bafd8e67c2e345
DavidMikeSimon <david.mike.simon@gmail.com>
2007-07-26 07:01:30 +0000

    Continuous boosting

--- avatar.py
+++ avatar.py
@@ -18,12 +18,11 @@ class DAvatar(drive.Drive):
 	lantern_rot_speed -- How fast (in revolutions per second) the lantern
 		can move around Satyrn to the player-desired position. Purely
 		visual; lantern's actual position changes instantaneously.
-	charge_push -- How hard to push for each pulse.
-	stall_diff -- How much speed can be lost for each stall.
-	cruise_push -- How hard to push each step when directional buttons are
-		pressed without charging up.
-	cruise_speed -- The maximum linear speed (in terms of body.getLinearVel) that
-		Satyrn can reach without doing a charged boost.
+	lantern_rad -- Radius of the circle that lantern moves around.
+	lantern_ang -- Current angle of the lantern from Satyrn, controlled by player.
+	boost_push -- How hard to push when boosting.
+	boost_max_speed -- The maximum linear speed (in terms of body.getLinearVel) that
+		Satyrn can reach while boosting.
 	"""
 	
 	def __init__(self):
@@ -77,16 +76,16 @@ class DAvatar(drive.Drive):
 				"lantern":image.DImage("satyrn/lantern.png", Size(0.25, 0.25)),
 			}, {
 				"lantern":sprite.DSprite.Anim([("lantern", 100)], "REPEAT"),
-			},
-			offset = Point(-0.3, 0)
+			}
 		)
 		self.lantern_rot_speed = 2
-		self.charge_push = 60
-		self.stall_diff = 3
-		self.cruise_push = 2
-		self.cruise_speed = 1
+		self.lantern_rad = 0.3
+		self.lantern_ang = 0.5
+		self.boost_push = 2
+		self.boost_max_speed = 5
 	
 	def _draw(self, obj):
+		self.lantern.offset = Point(self.lantern_rad, 0).rot(Point(0,0), self.lantern_ang-obj.ang)
 		self.lantern.draw(obj)
 		self.sprite.draw(obj)
 	
@@ -94,7 +93,7 @@ class DAvatar(drive.Drive):
 		# Add strong angular drag; Satyrn has a tendency to make himself stop rotating
 		angVel = obj.body.getAngularVel()[2]
 		obj.body.addTorque((0, 0, -angVel/4))
-		
+
 		# Figure out which direction the user is pressing in
 		push_vec = Point(0, 0)
 		if app.keys[K_UP]:
@@ -105,65 +104,47 @@ class DAvatar(drive.Drive):
 			push_vec[1] = 1
 		if app.keys[K_RIGHT]:
 			push_vec[0] = 1
+
+		# This will be the angle that the lantern moves towards, relative to Satyrn
+		# By default, until the player hits a button, the lantern just wants to be where it is
+		des_angle = self.lantern_ang
+		
+		# Used to figure out if we are going so slow that we need to fake the end of the stall by just zeroing velocity
 		
 		if push_vec[0] != 0 or push_vec[1] != 0:
 			# Rotate the lantern around towards the opposite side of where the player is pressing
 			# That's because, conceptually, the player should think of the lantern as pushing Satyrn
-			cur_angle = Point(0,0).ang_to(self.lantern.offset) % 1
-			des_angle = Point(0,0).ang_to(-push_vec) % 1
-			dist = (des_angle - cur_angle) % 1
-			if 1-dist < dist:
-				dist = -(1-dist)
-			if abs(dist) > self.lantern_rot_speed/app.maxfps:
-				dir = 1
-				if dist < 0:
-					dir = -1
-				dist = dir * self.lantern_rot_speed/app.maxfps
-			self.lantern.offset = self.lantern.offset.rot(Point(0,0), dist)
-		
-		# Figure out if the player just released the boost key
-		boost_released = False
-		for event in app.events:
-			if event.type == KEYUP and event.key == K_c:
-				# If the charge key was just released, then do a boost
-				boost_released = True
-				break
-		
-		if app.keys[K_c]:
-			# If the charge key is held down, then arrow keys just allow finetuning direction
-			# Float-charge-buffer is so that tapping boost key doesn't give us the charge animation
-			if self.sprite.cur_anim != "float-charge" and self.sprite.cur_anim != "float-charge-buffer":
-				self.sprite.cur_anim = "float-charge-buffer"
-		else:
-			# If we've stopped charging, then change the animation to show it
-			if self.sprite.cur_anim == "float-charge":
-				self.sprite.cur_anim = "float"
-			
-			if push_vec[0] != 0 or push_vec[1] != 0:
-				# If the boost key is not held down, then arrow keys are for movement
-				
-				if boost_released:
-					# Release a boost
-					self.sprite.cur_anim = "float-boost"
-					push_vec = push_vec.to_length(self.charge_push)
-					obj.body.addForce(push_vec.fake_3d_tuple())
-				else:
-					# If we didn't do a boost, then arrow keys mean cruising
-					# FIXME: Cap velocity on this
-					if self.sprite.cur_anim != "float-boost":
-						self.sprite.cur_anim = "float-cruise"
-					push_vec = push_vec.to_length(self.cruise_push)
-					obj.body.addForce(push_vec.fake_3d_tuple())
-			elif boost_released:
-				# If the boost key was tapped without arrow keys, that is for stalling
-				# Using setLinearVel is dumb, but I couldn't seem to get it accurate with addForce
+			des_angle = Point(0,0).ang_to(-push_vec)
+			if app.keys[K_b]:
 				self.sprite.cur_anim = "float-boost"
-				vel = Point(obj.body.getLinearVel()[0], obj.body.getLinearVel()[1])
-				speed = abs(vel.dist_to(Point(0,0)))
-				if speed <= self.stall_diff:
-					vel = Point(0,0)
-				else:
-					new_speed = speed - self.stall_diff
-					vel[0] = (new_speed/speed)*vel[0]
-					vel[1] = (new_speed/speed)*vel[1]
-				obj.body.setLinearVel(vel.fake_3d_tuple())
+				obj.body.addForce(Point(self.boost_push,0).rot(Point(0,0), self.lantern_ang+0.5).fake_3d_tuple())
+			else:
+				self.sprite.cur_anim = "float"
+		elif app.keys[K_b]:
+			# If boosting, but not holding a direction, point the lantern such that Satyrn slows down
+			des_angle = Point(0,0).ang_to(Point(obj.body.getLinearVel()[0], obj.body.getLinearVel()[1]))
+			if abs(des_angle%1-self.lantern_ang%1) < 0.01:
+				# Only start stalling if we're actually facing in the right direction
+				self.sprite.cur_anim = "float-boost"
+				obj.body.addForce(Point(self.boost_push,0).rot(Point(0,0), self.lantern_ang+0.5).fake_3d_tuple())
+			else:
+				self.sprite.cur_anim = "float"
+		
+		# Move the lantern towards the pressed direction
+		dist = (des_angle - self.lantern_ang) % 1
+		if 1-dist < dist:
+			dist = -(1-dist)
+		if abs(dist) > self.lantern_rot_speed/app.maxfps:
+			dir = 1
+			if dist < 0:
+				dir = -1
+			dist = dir * self.lantern_rot_speed/app.maxfps
+		self.lantern_ang += dist
+
+		# Figure out if the player just released the boost key
+		#boost_released = False
+		#for event in app.events:
+		#	if event.type == KEYUP and event.key == K_b:
+		#		# If the charge key was just released, then do a boost
+		#		boost_released = True
+		#		break
